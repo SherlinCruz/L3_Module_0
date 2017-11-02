1fe24e9a899db53ae35c35c66a531ddfe6fa7e20
dacoolkid <league@WTS-IM-17.attlocal.net>
2017-11-01 18:28:17 -0700

    November 1st, 2017
    I worked on Log Search. I am still working on it(I’m in the second
    instruction).

--- src/IntroToHashMaps/LogSearch.java
+++ src/IntroToHashMaps/LogSearch.java
@@ -3,10 +3,11 @@ package IntroToHashMaps;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import org.omg.CORBA.portable.ValueInputStream;
 
 public class LogSearch implements ActionListener {
 	/*
@@ -29,39 +30,88 @@ public class LogSearch implements ActionListener {
 	 * the user that the ID is not in the list.
 	 *
 	 */
-
-	// Crate a HashMap of Integers for the keys and Strings for the values. Create a
-	// GUI with three buttons.
-
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JButton AddName = new JButton();
 	JButton button1 = new JButton();
 	JButton button2 = new JButton();
 	JButton button3 = new JButton();
 
-	public static void main(String[] args) {
+	// int idNumber;
+	// String name;
+	// int searchId;
+	// String searchName;
 
-		HashMap<Integer, String> values = new HashMap<Integer, String>();
+	HashMap<Integer, String> values = new HashMap<Integer, String>();
+
+	public static void main(String[] args) {
+		LogSearch LogSearch = new LogSearch();
 
 	}
 
 	LogSearch() {
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+
 		frame.setVisible(true);
 		panel.setVisible(true);
 		button1.setVisible(true);
 		button2.setVisible(true);
 		button3.setVisible(true);
 
+		button1.setText("Add id & name");
+		button2.setText("Search id & name");
+		button3.setText("Show List");
 		button1.addActionListener(this);
 		button2.addActionListener(this);
 		button3.addActionListener(this);
+
+		frame.pack();
 
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		if (e.getSource().equals(button1)) {
+			String id = JOptionPane.showInputDialog("Enter an id.");
+
+			int idNumber = Integer.parseInt(id);
+
+			String name = JOptionPane.showInputDialog("Enter a name.");
+
+			values.put(idNumber, name);
+
+		} else if (e.getSource().equals(button2)) {
+			String searchId = JOptionPane.showInputDialog("Enter an id number.");
+
+			int searchIdNumber = Integer.parseInt(searchId);
+
+			String searchName = JOptionPane.showInputDialog("Enter a name.");
+
+			
+			
+	/*		
+			if (values.containsKey(searchIdNumber)) {
+
+				values.get(searchId);
+				System.out.println(searchId);
+				
+				
+				if (values.containsKey(searchName)) {
+					//values.get(name);
+
+					System.out.println(searchName);
+					
+				}
+
+			}*/
+
+			else {
+				System.out.println("entry does not exist.");
+			}
+		}
+
 	}
 }
